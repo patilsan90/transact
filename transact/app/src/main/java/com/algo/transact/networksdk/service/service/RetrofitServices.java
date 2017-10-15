53bea94a6a005e15d0d480c5ee30d3ab3b106c5f
WIN011-KAPIL\Kapil <kdev@rdalabs.com>
2017-10-15 15:30:35 +0530

    This interface contains all localmethods with the specified path(specific api call) to call api

--- transact/app/src/main/java/com/algo/transact/networksdk/service/service/RetrofitServices.java
+++ transact/app/src/main/java/com/algo/transact/networksdk/service/service/RetrofitServices.java
@@ -1,12 +1,9 @@
 package com.algo.transact.networksdk.service.service;
 
-import com.algo.transact.networksdk.service.LogingInterceptor;
-
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.Body;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
@@ -18,7 +15,9 @@ public interface RetrofitServices {
     @Headers({
             "Content-Type: application/json"
     })
-
+/*
+Here we have to add the all methods to call the web service
+* */
     //ToDo Remove this dummy api
     // This is just a dummy api name
     @POST("api/user/token")
