117059f2371c26b9cdf753e925f5249954b2a8d3
WIN011-KAPIL\Kapil <kapildv9999@gmail.com>
2017-10-15 19:52:51 +0530

    Add retrofit module

---
+++ transact/app/src/main/java/com/algo/transact/retrofit/transactcall/TransactCallImpl.java
@@ -0,0 +1,23 @@
+package com.algo.transact.retrofit.transactcall;
+
+import android.content.Context;
+
+import com.algo.transact.networksdk.service.TransactControllerFactory;
+
+/**
+ * Created by Kapil on 15/10/2017.
+ */
+
+public class TransactCallImpl extends TransactCall {
+    private TransactControllerFactory transactControllerFactory;
+
+    public TransactCallImpl(TransactControllerFactory transactControllerFactory) {
+        this.transactControllerFactory = transactControllerFactory;
+    }
+
+    @Override
+    public void callLogin() {
+//        transactControllerFactory.callLogin();
+    }
+
+}
