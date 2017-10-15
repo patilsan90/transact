dfc6cb75b2f3ec9e58da556bf419eb060366dfaf
WIN011-KAPIL\Kapil <kdev@rdalabs.com>
2017-10-15 15:25:59 +0530

    This module is responsible for all request response of all api's This class will decide whether the call is success or failure
    Signed-off-by: WIN011-KAPIL\Kapil <kdev@rdalabs.com>

---
+++ transact/app/src/main/java/com/algo/transact/networksdk/service/base/TransactManager.java
@@ -0,0 +1,11 @@
+package com.algo.transact.networksdk.service.base;
+
+/**
+ * Created by Kapil on 15/10/2017.
+ */
+
+public abstract class TransactManager {
+    /*Here In the params we should pass the required callback\
+    / listener and the required info to call the api.*/
+    public abstract void callLogin();
+}
